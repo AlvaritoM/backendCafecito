bf36177dc41bdfbe56283c98a7f4696e55a91ef0
Alvaro Molina <alvaromolina90@gmail.com>
2023-08-23 21:22:30 -0300

    agregamos una variable para agregar un puerto

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from "express";
 
-console.log("esto es una prueba");
+// console.log("esto es una prueba 1");
 // 1- Configuraciones Iniciales (puerto por ejemplo)
-
+const app = express();
+// crear una variable con express
+app.set("port", process.env.PORT || 4000);
+// esto seria para cuando el servidor manda el puerto en variable de entorno
 // 2- middlewares
 
 // 3- crear las rutas
