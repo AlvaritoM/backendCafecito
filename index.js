068cc464ba2b9d40f422bcd4e7eae6061a90ee8b
Alvaro Molina <alvaromolina90@gmail.com>
2023-08-30 21:31:39 -0300

    Agregamos middlewares y rutas

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import "dotenv/config"; // cuando subamos a produccion podra usar variables de entorno
+import productoRouter from "./src/routes/productos.routes.js";
 
 // console.log("esto es una prueba 1");
 // 1- Configuraciones Iniciales (puerto por ejemplo)
@@ -9,6 +13,14 @@ app.listen(app.get("port"), () => {
   console.log("Hola mundo " + app.get("port"));
 });
 // esto seria para cuando el servidor manda el puerto en variable de entorno
-// 2- middlewares
+// 2- middlewares: funciones que se agregan antes de las rutas
+app.use(cors()); // permite conexiones remotas
+app.use(express.json()); // interpreta JSON para cuando mandan solicitudes en este formato
+app.use(express.urlencoded({ extended: true })); // entienda array o string de las peticiones del body del request
+app.use(morgan("dev")); // nos da mas informacion en la terminal
 
 // 3- crear las rutas
+// http://localhost:4000/api/producto
+app.use("/api", productoRouter);
+
+//tarea crear ruta y controlador para los usuarios
---
+++ src/database/database.js
@@ -0,0 +1,1 @@
+// mongodb+srv://<AlvaritoM>:<nolase90>@cluster0.upcfref.mongodb.net/
