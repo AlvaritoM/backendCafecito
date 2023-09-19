c0182614a84ee4fbdc3f45c199aa6d3cd46d0f55
Alvaro Molina <alvaromolina90@gmail.com>
2023-09-19 20:39:27 -0300

    agregamos un producto en la bd

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -7,3 +7,15 @@ export const listarProductos = (req, res) => {
     console.log(error);
   }
 };
+
+export const crearProducto = async (req, res) => {
+  try {
+    console.log(req.body);
+    const productoNuevo = new Producto(req.body);
+    await productoNuevo.save();
+    res.status(201).json({ mensaje: "El producto fue creado correctamente" });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ mensaje: "El producto no pudo ser creado" });
+  }
+};
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { listarProductos } from "../controllers/productos.controllers.js";
+import { crearProducto } from "../controllers/productos.controllers.js";
 
 const router = Router();
-router.route("/producto").get(listarProductos);
+router.route("/producto").get(listarProductos).post(crearProducto);
 
 export default router;
